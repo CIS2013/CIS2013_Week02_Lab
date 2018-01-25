27901f243de3d1d330111829b833d40824585c69
BobbyRockers <rrockers@gmail.com>
2018-01-24 20:24:20 -0600

    While loops, and more conditionals.  Also chars.

--- examples.cpp
+++ examples.cpp
@@ -8,26 +8,37 @@ int main(){
 	// float room_radius = 10.0;
 	// float room_circle_area = (room_radius * room_radius) * 3.14;
 	bool keep_running = true;
+	char question = 'y';
 	
-	cout << "Enter your length: ";
-	cin >> room_length;
+	while(keep_running){
 	
-	cout << "Enter your width: ";
-	cin >> room_width;
-	
-	// == != > < <= >=
-	if(room_length == room_width){
-		cout << "Length and Width are the same" << endl;
-	} else {
-		cout << "Length and Width are NOT the same" << endl;
-		cout << "Length is: " << room_length << endl;
-		cout << "Width is: " << room_width << endl;
-	}
-	
-	if(room_length > room_width){
-		cout << "Length is greater than width" << endl;
-	} else {
-		cout << "Width is greater than length" << endl;
+		cout << "Enter your length: ";
+		cin >> room_length;
+		
+		cout << "Enter your width: ";
+		cin >> room_width;
+		
+		/* 
+			== equal
+		  != not equal
+			>  greater than 
+			<  less than
+			<=  less than or equal
+			>= greater than or equal
+			|| left or right is true 
+			&& BOTH left and right is true 
+		*/
+		
+		if(room_length > room_width){
+			cout << "Length is greater than width" << endl;
+		} else {
+			cout << "Width is greater than length" << endl;
+		}
+		
+		cout << "Run again (Y/n): ";
+		cin >> question;
+		
+		if(question != 'y'){ keep_running = false; }
 	}
 	
 	return 0;
